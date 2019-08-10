05fa80163d9f87fa62f4af839a30e07c545aa822
Justin Schell <justin@gudtech.com>
2019-08-09 17:30:24 -0700

    latest updates

--- kgpw.pl
+++ kgpw.pl
@@ -6,48 +6,70 @@ use POSIX qw(floor);
 use Getopt::Std;
 
 my %_OPTS;
-getopts('v', \%_OPTS);
+getopts('vi:', \%_OPTS);
+my $_INTERVAL = $_OPTS{i} // 10;
 
 my %_PODS;
 my %_STATUS_ABBR = (
-    Running => 'Run',
-    CrashLoopBackOff => 'CLB',
-    Terminating => 'Trm',
-    Pending => 'Pnd',
-    Evicted => 'Evc',
     Completed => 'Cmp',
-    RunContainerError => 'RCE',
     ContainerCreating => 'Cre',
-    PodInitializing => 'Ini',
+    CrashLoopBackOff => 'CLB',
     CreateContainerConfigError => 'CCC',
+    Error => 'Err',
+    ErrImagePull => 'EIP',
+    Evicted => 'Evc',
+    Init => 'Int',
+    NodeLost => 'NoL',
     OOMKilled => 'OOM',
+    Pending => 'Pnd',
+    PodInitializing => 'Ini',
+    RunContainerError => 'RCE',
+    Running => 'Run',
+    Terminating => 'Trm',
+    Unknown => 'Unk',
+    ImagePullBackOff => 'IPB',
+    ContainerCannotRun => 'CCR',
 );
 my %_STATUS_BACKGROUND_COLOR = (
     0 => {
-        Running => "\e[48;2;0;120;0m",
-        CrashLoopBackOff => "\e[48;2;170;0;170m",
-        Terminating => "\e[48;2;120;0;0m",
-        Pending => "\e[48;2;20;60;20m",
-        Evicted => "\e[48;2;50;50;50m",
         Completed => "\e[48;2;0;0;120m",
-        RunContainerError => "\e[48;2;170;0;170m",
         ContainerCreating => "\e[48;2;0;170;170m",
-        PodInitializing => "\e[48;2;0;170;170m",
+        CrashLoopBackOff => "\e[48;2;170;0;170m",
         CreateContainerConfigError => "\e[48;2;170;0;170m",
+        Error => "\e[48;2;170;0;170m",
+        ErrImagePull => "\e[48;2;170;0;170m",
+        Evicted => "\e[48;2;50;50;50m",
+        Init => "\e[48;2;20;60;20m",
+        NodeLost => "\e[48;2;170;0;170m",
         OOMKilled => "\e[48;2;170;0;170m",
+        Pending => "\e[48;2;20;60;20m",
+        PodInitializing => "\e[48;2;0;170;170m",
+        RunContainerError => "\e[48;2;170;0;170m",
+        Running => "\e[48;2;0;120;0m",
+        Terminating => "\e[48;2;120;0;0m",
+        Unknown => "\e[48;2;170;0;170m",
+        ImagePullBackOff => "\e[48;2;170;0;170m",
+        ContainerCannotRun => "\e[48;2;170;0;170m",
     },
     1 => {
-        Running => "\e[48;2;20;140;20m",
-        CrashLoopBackOff => "\e[48;2;190;20;190m",
-        Terminating => "\e[48;2;140;20;20m",
-        Pending => "\e[48;2;40;80;40m",
-        Evicted => "\e[48;2;70;70;70m",
         Completed => "\e[48;2;20;20;140m",
-        RunContainerError => "\e[48;2;190;20;190m",
         ContainerCreating => "\e[48;2;20;190;190m",
-        PodInitializing => "\e[48;2;20;190;190m",
+        CrashLoopBackOff => "\e[48;2;190;20;190m",
         CreateContainerConfigError => "\e[48;2;190;20;190m",
+        Error => "\e[48;2;190;20;190m",
+        ErrImagePull => "\e[48;2;190;20;190m",
+        Evicted => "\e[48;2;70;70;70m",
+        Init => "\e[48;2;40;80;40m",
+        NodeLost => "\e[48;2;190;20;190m",
         OOMKilled => "\e[48;2;190;20;190m",
+        Pending => "\e[48;2;40;80;40m",
+        PodInitializing => "\e[48;2;20;190;190m",
+        RunContainerError => "\e[48;2;190;20;190m",
+        Running => "\e[48;2;20;140;20m",
+        Terminating => "\e[48;2;140;20;20m",
+        Unknown => "\e[48;2;190;20;190m",
+        ImagePullBackOff => "\e[48;2;190;20;190m",
+        ContainerCannotRun => "\e[48;2;190;20;190m",
     },
 );
 my %_STATUS_MAXES;
@@ -66,74 +88,63 @@ my $_CLEAR_REST_AND_NEWLINE = "$_CLEAR_REST\n";
 my $_RESET_COLORS = "\e[0m";
 
 my $order = 0;
-my $last_num_lines = 0;
-while (<STDIN>) {
-    chomp;
-    my ($full_name, $container_status, $pod_status, $restarts, $age) = split /\s+/, $_;
-    next
-        if $full_name eq 'NAME';
+while (1) {
+    my %new_pods;
 
-    $full_name =~ /^(?<prefix>.+)-(?<id>.+)$/;
-    my $pod_prefix = $+{prefix};
-    my $pod_id = $+{id};
-    $_PODS{$pod_prefix}{$pod_id} = {
-        prefix => $pod_prefix,
-        id => $pod_id,
-        container_status => $container_status,
-        pod_status => $pod_status,
-        restarts => $restarts,
-        age => $age,
-        order => $_PODS{$pod_prefix}{$pod_id}{order} // $order++,
-    };
+    my $cmd_output = `kubectl get pods`;
+    chomp $output;
 
-    my ($output, $lines) = render($pod_prefix, $pod_id);
-    _move_up_lines($last_num_lines);
+    foreach my $line (split /\n/, $cmd_output) {
+        my ($full_name, $container_status, $pod_status, $restarts, $age) = map { (split ':', $_)[0] } split /\s+/, $line;
+        next
+            if $full_name eq 'NAME';
 
-    $last_num_lines = $lines;
+        my ($pod_prefix, $pod_id);
+        if ($full_name =~ /-/) {
+            $full_name =~ /^(?<prefix>.+)-(?<id>.+)$/;
+            $pod_prefix = $+{prefix};
+            $pod_id = $+{id};
+        }
+        else {
+            $pod_prefix = $full_name;
+            $pod_id = '';
+        }
 
-    print STDERR $output;
+        $new_pods{$pod_prefix}{$pod_id} = {
+            prefix => $pod_prefix,
+            id => $pod_id,
+            container_status => $container_status,
+            pod_status => $pod_status,
+            restarts => $restarts,
+            age => $age,
+            order => $_PODS{$pod_prefix}{$pod_id}{order} // $order++,
+        };
+
+        $new_pods{$pod_prefix}{$pod_id}{is_updated}
+            = grep { "$_PODS{$pod_prefix}{$pod_id}{$_}" ne "$new_pods{$pod_prefix}{$pod_id}{$_}" }
+                grep { $_ ne 'age' } keys %{$new_pods{$pod_prefix}{$pod_id}};
+    }
+
+    %_PODS = %new_pods;
+
+    _print_new_output(&{$_OPTS{v} ? _render_pods_verbose : _render_pods_compact}(_group_pods()));
+
+    sleep $_INTERVAL;
 }
 
 exit 0;
 
-sub render {
-    my ($updated_pod_prefix, $updated_pod_id) = @_;
-
-    return $_OPTS{v}
-        ? _render_pods_verbose(_analyze_pods(), $updated_pod_prefix, $updated_pod_id)
-        : _render_pods_compact(_analyze_pods(), $updated_pod_prefix, $updated_pod_id);
-}
-
-sub _analyze_pods {
+sub _group_pods {
     my @groups;
     foreach my $pod_prefix (sort { $a cmp $b } keys %_PODS) {
-        $length_maxes{header} = max($length_maxes{header} // 0, length($pod_prefix));
         my @statuses = ({
             header => $pod_prefix,
         });
 
         foreach my $pod_id (sort {
             $_PODS{$pod_prefix}{$a}{order} <=> $_PODS{$pod_prefix}{$b}{order}
-        } keys $_PODS{$pod_prefix}) {
-            my $status = $_PODS{$pod_prefix}{$pod_id};
-
-            %length_maxes = (
-                %length_maxes,
-                id => max($length_maxes{id} // 0, length($status->{id})),
-                container_status => max(
-                    $length_maxes{container_status} // 0, length($status->{container_status})),
-                pod_status => max(
-                    $length_maxes{pod_status} // 0,
-                    length($_STATUS_ABBR{$status->{pod_status}} // $status->{pod_status}),
-                ),
-                restarts => max($length_maxes{restarts} // 0, length($status->{restarts})),
-                age => max(
-                    $length_maxes{age} // 0,
-                    length($status->{age} eq '<invalid>' ? 'x' : $status->{age}),
-                ),
-            );
-
-            push @statuses, { pod => $status };
+        } keys %{$_PODS{$pod_prefix}}) {
+            push @statuses, { pod => $_PODS{$pod_prefix}{$pod_id} };
         }
 
         push @groups, \@statuses;
@@ -142,55 +153,45 @@ sub _analyze_pods {
     return \@groups;
 }
 
-sub _move_up_lines {
-    my ($num_lines) = @_;
+sub _print_new_output {
+    # Move to home position, clear screen, print output
+    print STDERR "\e[H\e[J", shift;
+}
 
-    print STDERR "\e[", $num_lines, "A"
-        if $num_lines;
+sub _render_aggregation_compact {
+    my ($aggregation, $num_lines, %length_maxes) = @_;
 
-    return;
+    my $average_age = $aggregation->{age} / $aggregation->{count};
+    $average_age = $average_age > 60 * 60 * 24 ? sprintf("%.0f", $average_age / (60 * 60 * 24)) . 'd'
+        : $average_age > 60 * 60 ? sprintf("%.0f", $average_age / (60 * 60)) . 'h'
+        : $average_age > 60 ? sprintf("%.0f", $average_age / 60) . 'm'
+        : sprintf("%.0f", $average_age) . 's';
+
+    return ($aggregation->{is_updated}
+                      ? $_HIGHLIGHT_FOREGROUND_COLOR : $_DEFAULT_FOREGROUND_COLOR)
+        . $_STATUS_BACKGROUND_COLOR{($num_lines + $num_aggregations) % 2 == 0 ? 1 : 0}{$aggregation->{pod_status}} . ' '
+        . sprintf(
+            "%$length_maxes{pod_status}s",
+            $_STATUS_ABBR{$aggregation->{pod_status}} || $aggregation->{pod_status},
+        ) . ' '
+            . sprintf("%$length_maxes{count}s", $aggregation->{count}) . ' '
+            . sprintf("%$length_maxes{total_containers}s", $aggregation->{running_containers}) . '/'
+            . sprintf("%$length_maxes{total_containers}s", $aggregation->{total_containers}) . ' '
+            . sprintf("%$length_maxes{restarts}s", $aggregation->{restarts}) . ' '
+            . sprintf("%3s", $average_age) . ' ';
 }
 
 sub _render_pods_compact {
-    my ($groups, $updated_pod_prefix, $updated_pod_id) = @_;
-
-    my $buffer = '';
-    my $first_line = 1;
-    my $num_lines = 0;
+    my ($groups) = @_;
 
     my %group_aggregations;
     foreach my $group (@$groups) {
         my $group_header = shift @$group;
 
         foreach my $status (@$group) {
-            my $aggregation = $group_aggregations{$group_header->{header}}{$status->{pod}{pod_status}} ||= {
-                count => 0,
-                pod_status => $status->{pod}{pod_status},
-                running_containers => 0,
-                total_containers => 0,
-                restarts => 0,
-            };
-
-            $aggregation->{count}++;
-
-            my ($running_containers, $total_containers)
-                = split qr,\s*/\s*,, $status->{pod}{container_status};
-            $aggregation->{running_containers} += $running_containers;
-            $aggregation->{total_containers} += $total_containers;
-
-            $aggregation->{restarts} += $status->{pod}{restarts};
-
-            $aggregation->{age} += $status->{pod}{age} =~ /^(?<seconds>\d+)s$/ ? $+{seconds}
-                : $status->{pod}{age} =~ /^(?<minutes>\d+)m$/ ? $+{minutes} * 60
-                : $status->{pod}{age} =~ /^(?<hours>\d+)h$/ ? $+{hours} * 60 * 60
-                : $status->{pod}{age} =~ /^(?<days>\d+)d$/ ? $+{days} * 60 * 60 * 24
-                : die "unrecognized age: $status->{pod}{age}\n";
+            _update_aggregation(\%group_aggregations, $group_header, $status);
         }
     }
-
-    # p($groups);
-    # p(%group_aggregations);
-    # die;
 
     my %length_maxes;
     foreach my $prefix (keys %group_aggregations) {
@@ -207,34 +208,16 @@ sub _render_pods_compact {
     my $num_lines = 0;
 
     foreach my $prefix (sort { $a cmp $b } keys %group_aggregations) {
-        $buffer .= ($prefix eq $updated_pod_prefix ? $_HIGHLIGHT_FOREGROUND_COLOR : $_DEFAULT_FOREGROUND_COLOR)
-            . $_DEFAULT_BACKGROUND_COLOR{$num_lines % 2 == 0 ? 1 : 0}
+        $buffer .= $_DEFAULT_FOREGROUND_COLOR
             . sprintf "%-$length_maxes{prefix}s ", $prefix;
 
         my $num_aggregations = 0;
         foreach my $aggregation (sort { $a->{pod_status} cmp $b->{pod_status} } values %{$group_aggregations{$prefix}}) {
-            my $average_age = $aggregation->{age} / $aggregation->{count};
-            $average_age = $average_age > 60 * 60 * 24 ? sprintf("%.0f", $average_age / (60 * 60 * 24)) . 'd'
-                : $average_age > 60 * 60 ? sprintf("%.0f", $average_age / (60 * 60)) . 'h'
-                : $average_age > 60 ? sprintf("%.0f", $average_age / 60) . 'm'
-                : sprintf("%.0f", $average_age) . 's';
-
-            $buffer .= $_DEFAULT_FOREGROUND_COLOR
-                . $_STATUS_BACKGROUND_COLOR{($num_lines + $num_aggregations) % 2 == 0 ? 1 : 0}{$aggregation->{pod_status}} . ' '
-                . sprintf(
-                    "%$length_maxes{pod_status}s",
-                    $_STATUS_ABBR{$aggregation->{pod_status}} || $aggregation->{pod_status},
-                ) . ' '
-                . sprintf("%$length_maxes{count}s", $aggregation->{count}) . ' '
-                . sprintf("%$length_maxes{total_containers}s", $aggregation->{running_containers}) . '/'
-                . sprintf("%$length_maxes{total_containers}s", $aggregation->{total_containers}) . ' '
-                . sprintf("%$length_maxes{restarts}s", $aggregation->{restarts}) . ' '
-                . sprintf("%3s", $average_age) . ' ';
-
+            $buffer .= _render_aggregation_compact($aggregation, $num_lines, %length_maxes);
             $num_aggregations++;
         }
 
-        $buffer .= "$_RESET_COLORS$_CLEAR_REST_AND_NEWLINE$_RESET_COLORS";
+        $buffer .= "$_RESET_COLORS$_CLEAR_REST_AND_NEWLINE$_RESET_COLORS$_CLEAR_REST";
         $num_lines++;
     }
 
@@ -242,7 +225,7 @@ sub _render_pods_compact {
 }
 
 sub _render_pods_verbose {
-    my ($groups, $updated_pod_prefix, $updated_pod_id) = @_;
+    my ($groups) = @_;
 
     my %length_maxes;
     foreach my $group (@$groups) {
@@ -308,8 +291,7 @@ sub _render_pods_verbose {
             }
             else {
                 $buffer .= (
-                    $status->{pod}{prefix} eq $updated_pod_prefix
-                        && $status->{pod}{id} eq $updated_pod_id
+                    $status->{pod}{is_updated}
                         ? $_HIGHLIGHT_FOREGROUND_COLOR
                         : $_DEFAULT_FOREGROUND_COLOR
                     ) . $_STATUS_BACKGROUND_COLOR{$i % 2 == 0 ? 1 : 0}{$status->{pod}{pod_status}}
@@ -333,8 +315,41 @@ sub _render_pods_verbose {
         }
     }
 
-    $buffer .= "$_RESET_COLORS$_CLEAR_REST_AND_NEWLINE$_RESET_COLORS";
+    $buffer .= "$_RESET_COLORS$_CLEAR_REST_AND_NEWLINE$_RESET_COLORS$_CLEAR_REST";
     $num_lines++;
 
-    return ($buffer, $num_lines);
+    return $buffer;
+}
+
+sub _update_aggregation {
+    my ($group_aggregations, $group_header, $status) = @_;
+
+    my $aggregation = $group_aggregations->{$group_header->{header}}{$status->{pod}{pod_status}} ||= {
+        count => 0,
+        pod_status => $status->{pod}{pod_status},
+        running_containers => 0,
+        total_containers => 0,
+        restarts => 0,
+        is_updated => 0,
+    };
+
+    $aggregation->{count}++;
+
+    my ($running_containers, $total_containers)
+        = split qr,\s*/\s*,, $status->{pod}{container_status};
+    $aggregation->{running_containers} += $running_containers;
+    $aggregation->{total_containers} += $total_containers;
+
+    $aggregation->{restarts} += $status->{pod}{restarts};
+
+    $aggregation->{age} += $status->{pod}{age} =~ /^(?<seconds>\d+)s$/ ? $+{seconds}
+        : $status->{pod}{age} =~ /^(?<minutes>\d+)m$/ ? $+{minutes} * 60
+        : $status->{pod}{age} =~ /^(?<hours>\d+)h$/ ? $+{hours} * 60 * 60
+        : $status->{pod}{age} =~ /^(?<days>\d+)d$/ ? $+{days} * 60 * 60 * 24
+        : die "unrecognized age: $status->{pod}{age}\n";
+
+    $aggregation->{is_updated} = 1
+        if $status->{pod}{is_updated};
+
+    return;
 }
