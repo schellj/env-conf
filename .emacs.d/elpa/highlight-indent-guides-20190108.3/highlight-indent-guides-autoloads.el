7100b930b149e28141afb3ca033cd53d7ed2038d
Justin Schell <justin@gudtech.com>
2020-07-21 14:17:36 -0700

    Ignore external package files

--- .emacs.d/elpa/highlight-indent-guides-20190108.3/highlight-indent-guides-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; highlight-indent-guides-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; highlight-indent-guides-autoloads.el ends here
